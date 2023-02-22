6280957537d1e6a33bf087e0e8b46ddc54116335
shio <85730998+dino3616@users.noreply.github.com>
2023-02-22 17:06:07 +0000

    chore: 🔧 (next) transpilePackagesの設定を追加

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   experimental: {
     fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
   },
+  transpilePackages: ['@yumemi-exam/resas-api'],
 };
 
 module.exports = removeImports()(nextConfig);

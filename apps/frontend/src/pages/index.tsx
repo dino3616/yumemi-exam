cc69bdf566b598eff201fad1d3d270181a366184
shio <85730998+dino3616@users.noreply.github.com>
2023-02-23 12:12:12 +0000

    feat: ✨ (frontend) prefectureとpopulation-compositionをisrで取得するように修正

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,5 +1,41 @@
-import type { NextPage } from 'next';
+import { resasClient } from '@yumemi-exam/resas-api';
+import type { Prefecture, PopulationComposition } from '@yumemi-exam/resas-api/model';
+import type { GetStaticProps, NextPage } from 'next';
 
-const RootPage: NextPage = () => <h1 className="text-primary">Hello, yumemi-exam World!</h1>;
+type RootPageProps = {
+  prefecturePopulationCompositions: {
+    prefecture: Prefecture;
+    boundaryYear: number;
+    populationCompositions: PopulationComposition[];
+  }[];
+};
+
+const RootPage: NextPage<RootPageProps> = () => <h1 className="text-primary">Hello, yumemi-exam World!</h1>;
+
+export const getStaticProps: GetStaticProps<RootPageProps> = async () => {
+  const { result: prefectures } = await resasClient.getPrefectures();
+
+  const prefecturePopulationCompositions = await Promise.all(
+    prefectures.map(async (prefecture) => {
+      const {
+        result: { boundaryYear, data: populationCompositions },
+      } = await resasClient.getPopulationComposition({
+        prefCode: prefecture.prefCode,
+        cityCode: '-',
+      });
+
+      return { prefecture, boundaryYear, populationCompositions };
+    }),
+  );
+
+  return {
+    props: {
+      prefecturePopulationCompositions: JSON.parse(
+        JSON.stringify(prefecturePopulationCompositions.sort((a, b) => a.prefecture.prefCode - b.prefecture.prefCode)),
+      ),
+    },
+    revalidate: 60 * 60 * 24,
+  };
+};
 
 export default RootPage;

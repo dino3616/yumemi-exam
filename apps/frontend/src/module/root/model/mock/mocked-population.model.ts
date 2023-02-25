43bcab42d2c142f21b9bcb661f25e6df3325553c
shio <85730998+dino3616@users.noreply.github.com>
2023-02-25 03:34:40 +0000

    feat: ✨ (root/model) Population modelのモックを作成

---
+++ apps/frontend/src/module/root/model/mock/mocked-population.model.ts
@@ -0,0 +1,26 @@
+import type { Population } from '@/module/root/model/population.model';
+
+export const mockedPopulation1: Population = {
+  year: 1980,
+  value: 12817,
+};
+
+export const mockedPopulation2: Population = {
+  year: 1985,
+  value: 12707,
+};
+
+export const mockedPopulation3: Population = {
+  year: 1990,
+  value: 12571,
+};
+
+export const mockedPopulation4: Population = {
+  year: 1995,
+  value: 12602,
+};
+
+export const mockedPopulation5: Population = {
+  year: 2000,
+  value: 12199,
+};

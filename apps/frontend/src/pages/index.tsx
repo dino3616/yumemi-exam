cf61cd7bfecf4de7079f25180f5ce73497fffddb
shio <85730998+dino3616@users.noreply.github.com>
2023-02-25 03:38:59 +0000

    refactor: ♻️ (pages/root) use-caseを使用する際にrepositoryをinjectするように修正

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,6 +1,8 @@
-import { resasClient } from '@yumemi-exam/resas-api';
 import type { Prefecture, PopulationComposition } from '@yumemi-exam/resas-api/model';
 import type { GetStaticProps, NextPage } from 'next';
+import { getPopulationComposition } from '@/module/root/repository/impl/get-population-composition.repository';
+import { getPrefectures } from '@/module/root/repository/impl/get-prefectures.repository';
+import { getPrefecturePopulationCompositions } from '@/module/root/use-case/impl/get-prefecture-population-compositions.use-case';
 
 type RootPageProps = {
   prefecturePopulationCompositions: {
@@ -13,20 +15,12 @@ type RootPageProps = {
 const RootPage: NextPage<RootPageProps> = () => <h1 className="text-primary">Hello, yumemi-exam World!</h1>;
 
 export const getStaticProps: GetStaticProps<RootPageProps> = async () => {
-  const { result: prefectures } = await resasClient.getPrefectures();
-
-  const prefecturePopulationCompositions = await Promise.all(
-    prefectures.map(async (prefecture) => {
-      const {
-        result: { boundaryYear, data: populationCompositions },
-      } = await resasClient.getPopulationComposition({
-        prefCode: prefecture.prefCode,
-        cityCode: '-',
-      });
-
-      return { prefecture, boundaryYear, populationCompositions };
-    }),
-  );
+  const prefecturePopulationCompositions = await getPrefecturePopulationCompositions({
+    inject: {
+      getPopulationComposition,
+      getPrefectures,
+    },
+  });
 
   return {
     props: {

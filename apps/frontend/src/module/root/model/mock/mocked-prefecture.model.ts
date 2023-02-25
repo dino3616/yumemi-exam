0073fce7fb807bc4a9477d5ab4ca1548daef616d
shio <85730998+dino3616@users.noreply.github.com>
2023-02-25 03:35:01 +0000

    feat: ✨ (root/model) Prefecture modelのモックを作成

---
+++ apps/frontend/src/module/root/model/mock/mocked-prefecture.model.ts
@@ -0,0 +1,26 @@
+import type { Prefecture } from '@/module/root/model/prefecture.model';
+
+export const mockedPrefecture1: Prefecture = {
+  prefCode: 1,
+  prefName: '北海道',
+};
+
+export const mockedPrefecture2: Prefecture = {
+  prefCode: 2,
+  prefName: '青森県',
+};
+
+export const mockedPrefecture3: Prefecture = {
+  prefCode: 3,
+  prefName: '岩手県',
+};
+
+export const mockedPrefecture4: Prefecture = {
+  prefCode: 4,
+  prefName: '宮城県',
+};
+
+export const mockedPrefecture5: Prefecture = {
+  prefCode: 5,
+  prefName: '秋田県',
+};

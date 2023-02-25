e637f37e7d63bfdecb01d5ea641f41fad1c1d720
shio <85730998+dino3616@users.noreply.github.com>
2023-02-25 07:51:30 +0000

    feat: ✨ (root/model) PrefecturePopulationComposition modelを作成

---
+++ apps/frontend/src/module/root/model/prefecture-population-composition.model.ts
@@ -0,0 +1,8 @@
+import type { PopulationComposition } from '@/module/root/model/population-composition.model';
+import type { Prefecture } from '@/module/root/model/prefecture.model';
+
+export type PrefecturePopulationComposition = {
+  prefecture: Prefecture;
+  boundaryYear: number;
+  populationCompositions: PopulationComposition[];
+};

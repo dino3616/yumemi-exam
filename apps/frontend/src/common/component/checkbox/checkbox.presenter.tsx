b101ee628ad8120f35de772509f77ab03b9d79a5
shio <85730998+dino3616@users.noreply.github.com>
2023-02-25 14:56:22 +0000

    refactor: ♻️ (common/checkbox) propsの順序関係を修正

--- apps/frontend/src/common/component/checkbox/checkbox.presenter.tsx
+++ apps/frontend/src/common/component/checkbox/checkbox.presenter.tsx
@@ -7,10 +7,9 @@ type RootProps = ComponentPropsWithoutRef<typeof RadixUiCheckbox.Root> &
 
 const Root: FC<RootProps> = ({ children, className, ...props }) => (
   <RadixUiCheckbox.Root
-    {...props}
-    aria-label={props['aria-label']}
     style={{ WebkitTapHighlightColor: 'transparent', cursor: 'pointer' }}
     className={twMerge('flex h-6 w-6 items-center justify-center rounded-lg bg-white shadow-inner transition-all hover:bg-accent-200/80', className)}
+    {...props}
   >
     {children}
   </RadixUiCheckbox.Root>
@@ -19,7 +18,7 @@ const Root: FC<RootProps> = ({ children, className, ...props }) => (
 type IndicatorProps = ComponentPropsWithoutRef<typeof RadixUiCheckbox.Indicator>;
 
 const Indicator: FC<IndicatorProps> = ({ children, className, ...props }) => (
-  <RadixUiCheckbox.Indicator {...props} className={twMerge('text-accent-500', className)}>
+  <RadixUiCheckbox.Indicator className={twMerge('text-accent-500', className)} {...props}>
     {children}
   </RadixUiCheckbox.Indicator>
 );

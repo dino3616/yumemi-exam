776f8988de0441e31500f640fe3c7983a30cbff1
shio <85730998+dino3616@users.noreply.github.com>
2023-02-26 14:54:55 +0000

    fix: 🐞 (common/graph) highchartsのaccessibility moduleを読み込むように修正

--- apps/frontend/src/common/component/graph/graph.presenter.tsx
+++ apps/frontend/src/common/component/graph/graph.presenter.tsx
@@ -1,6 +1,11 @@
 import Highcharts from 'highcharts';
+import highchartsAccessibility from 'highcharts/modules/accessibility';
 import HighchartsReact from 'highcharts-react-official';
 import { ComponentPropsWithoutRef, forwardRef, ForwardRefExoticComponent } from 'react';
+
+if (typeof window !== `undefined`) {
+  highchartsAccessibility(Highcharts);
+}
 
 export type GraphProps = ComponentPropsWithoutRef<typeof HighchartsReact> & {
   options: Highcharts.Options;

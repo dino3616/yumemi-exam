7a58f303f306edd291f2cfe6600f35db488b72c0
shio <85730998+dino3616@users.noreply.github.com>
2023-02-26 17:48:07 +0000

    test: 🧪 (root/use-case) 関心が薄い部分はmockをinjectするように修正

--- apps/frontend/src/module/root/use-case/impl/get-prefecture-total-population-compositions.spec.ts
+++ apps/frontend/src/module/root/use-case/impl/get-prefecture-total-population-compositions.spec.ts
@@ -1,5 +1,7 @@
-import { mockedPopulation1, mockedPopulation2, mockedPopulation3 } from '@/module/root/model/mock/mocked-population.model';
+import { mockedPopulationComposition1 } from '@/module/root/model/mock/mocked-population-composition.model';
 import { mockedPrefecture1, mockedPrefecture2, mockedPrefecture3 } from '@/module/root/model/mock/mocked-prefecture.model';
+import { mockedGetPopulationComposition1 } from '@/module/root/repository/mock/mocked-get-population-composition.repository';
+import { mockedGetPrefectures1 } from '@/module/root/repository/mock/mocked-get-prefectures.repository';
 import { getPrefectureTotalPopulationCompositions } from '@/module/root/use-case/impl/get-prefecture-total-population-compositions.use-case';
 
 describe('getPrefectureTotalPopulationCompositions', () => {
@@ -11,15 +13,7 @@ describe('getPrefectureTotalPopulationCompositions', () => {
           { ...mockedPrefecture2, prefCode: 3 },
           { ...mockedPrefecture3, prefCode: 1 },
         ],
-        getPopulationComposition: async () => ({
-          boundaryYear: 2015,
-          populationCompositions: [
-            {
-              label: '総人口',
-              data: [mockedPopulation1, mockedPopulation2, mockedPopulation3],
-            },
-          ],
-        }),
+        getPopulationComposition: mockedGetPopulationComposition1,
       },
     });
 
@@ -32,17 +26,13 @@ describe('getPrefectureTotalPopulationCompositions', () => {
     expect(
       getPrefectureTotalPopulationCompositions({
         inject: {
-          getPrefectures: async () => [
-            { ...mockedPrefecture1, prefCode: 2 },
-            { ...mockedPrefecture2, prefCode: 3 },
-            { ...mockedPrefecture3, prefCode: 1 },
-          ],
+          getPrefectures: mockedGetPrefectures1,
           getPopulationComposition: async () => ({
             boundaryYear: 2015,
             populationCompositions: [
               {
+                ...mockedPopulationComposition1,
                 label: '年少人口',
-                data: [mockedPopulation1, mockedPopulation2, mockedPopulation3],
               },
             ],
           }),
